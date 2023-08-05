928bdde707171b20643eac96258321f48a138b3f
Vladuha <v.a.malinkin95@gmail.com>
2023-08-05 11:57:31 +0300

    newcommit

---
+++ C#/Program.cs
@@ -0,0 +1,58 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32( Console.ReadLine() );
+// int square = num * num;
+// Console.WriteLine(square);
+// Console.WriteLine($"Квадрат введенного числа равен {square}");
+// if (num > 0)
+// {
+//     System.Console.WriteLine("Это положительное число");
+// }
+// else if (num == 0)
+//     System.Console.WriteLine("Это ноль");
+// else
+//     System.Console.WriteLine("Это отрицательное число");
+
+
+
+// int i = 1;
+// while (i <= 10)
+// {
+//     Console.Write($"{i} ");
+//     i = i + 1;
+//     //i++;
+// }
+// System.Console.WriteLine();
+
+
+
+// System.Console.WriteLine("Введите 1 число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите 2 число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2 * num2)
+//     System.Console.WriteLine("да");
+// else
+//     System.Console.WriteLine("нет");
+
+
+
+// Console.Write("введите первое трехзначное целое число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = num % 10 ;
+// System.Console.WriteLine(i);
+
+
+
+// Console.Write("введите целое число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = -num ;
+// while (i <= num)
+// {
+//     Console.Write($"{i} ");
+//     i++ ; 
+// }
+// System.Console.WriteLine();
+
+
+
