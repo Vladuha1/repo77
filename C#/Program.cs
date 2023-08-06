485b05d155aea6f9ca0c9c0d6940dc9d2fbb37f5
Vladuha <v.a.malinkin95@gmail.com>
2023-08-06 11:24:42 +0300

    new commit

--- C#/Program.cs
+++ C#/Program.cs
@@ -119,3 +119,39 @@
 
 
 
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+// Console.WriteLine("Введите число num1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число num2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 == num2*num2 || num2 == num1 * num1)
+//     Console.WriteLine($"{num1} , {num2}-> да");
+// else
+//     Console.WriteLine($"{num1} , {num2}-> нет");
+
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+// Console.Write("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result1 = number % 7;
+// int result2 = number % 23;
+// if (result1 == 0 && result2 == 0)
+//     Console.WriteLine($"{number} -> да");
+// else
+//     Console.WriteLine($"{number} -> нет");
+
+
+
