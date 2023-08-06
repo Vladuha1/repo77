0b5fe4a53be62146a79d46ba936e96d64932daf1
Vladuha <v.a.malinkin95@gmail.com>
2023-08-06 10:24:40 +0300

    new commit

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,4 +1,8 @@
-﻿// // See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+
 // Console.WriteLine("Введите целое число");
 // int num = Convert.ToInt32( Console.ReadLine() );
 // int square = num * num;
@@ -56,3 +60,21 @@
 
 
 
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int num = new Random().Next(10,100);
+// System.Console.WriteLine($"Было сгенерировано число {num}");
+// int digit1 = num / 10;
+// int digit2 = num % 10;
+// if (digit1 > digit2)
+//     System.Console.WriteLine($"Наибольшая цифра этого числа это {digit1}");
+// else if (digit1 == digit2)
+//     System.Console.WriteLine("Обе цифры этого числа равны");
+// else
+//     System.Console.WriteLine($"Наибольшая цифра этого числа это {digit2}");
